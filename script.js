6dac320f8a13cf13b042ccdfd762e43c6f439b96
Vishal8848 <vishalpranav156@gmail.com>
2022-05-11 07:13:40 +0530

    Second Level Commit

--- script.js
+++ script.js
@@ -1,8 +1,40 @@
 let themes = [ "eclipse", "dracula" ], themeOption = 1;
-let done = [ false, false, false, false, false ]
+let done = [ false, false, false, false, false, false, false ]
 let questions = [
     {
         id: 1,
+        mode: "xml",
+        task: "Write something in the <div> tags and see what happens",
+        actual: `<style>\n#objective { font-family: 'Poppins'; text-align: start; }\n</style>\n<h4> First HTML Code: </h4>\n<hr>\n<div id="objective">\n<!-- Write anything here -->\n\n</div>`,
+        editable: `<!-- Basic HTML Template -->\n<h4> First HTML Code: </h4>\n<hr>\n<div id="objective">\n<!-- Write anything here -->\n\n</div>`,
+        build: function(code) {
+            return code;
+        }
+    },
+    {
+        id: 2,
+        mode: "css",
+        task: "A valuable ornament is hiding behind the rock. Move to reveal it",
+        actual: `<style>
+                #msg, #rock {
+                    position: absolute;
+                }
+                #rock {
+                    width: 150px;
+                    hright: 150px;
+                    /* Code */
+                }
+            </style>
+            <div id="msg" class="fw-bold text-success">YOU</div>
+            <img id="rock" src="rock.jpg" alt="Rock">`,
+        editable: `/* Try making some CSS transitions */\n#rock {\n\t/* Add your CSS here */\n}`,
+        build: function(code) {
+            code = code.substring(code.indexOf('{') + 1, code.indexOf('}'));
+            return this.actual.replace("/* Code */", code);
+        }
+    },
+    {
+        id: 3,
         mode: "css",
         task: "The Air Force signal team has reported that a flight is wobbling in mid air. We need your help to stop it. Write your CSS code that will get the job done.",
         actual: `<style>
@@ -16,20 +48,7 @@ let questions = [
         }
     },
     {
-        id: 2,
-        mode: "javascript",
-        task: "Being a detective, your objective is to find a hidden clue. You have a hint: jQuery makes JS better",
-        actual: `<script>
-            $("#objective").hide(); /* Code */
-            </script>
-            <div id="objective" class="fs-4 fw-bold text-center"> Every Bee is Above the Sea </div>`,
-        editable: `// If intel is $("#objective")... Then what's your JS\n`,
-        build: function(code) {
-            return this.actual.replace("/* Code */", code);
-        }
-    },
-    {
-        id: 3,
+        id: 4,
         mode: "css",
         task: "Can't see clear. Can you help me enlarge the content. Then make every character even, It looks clumsy.",
         actual: `<style>
@@ -43,7 +62,20 @@ let questions = [
         }
     },
     {
-        id: 4,
+        id: 5,
+        mode: "javascript",
+        task: "Being a detective, your objective is to find a hidden clue. You have a hint: jQuery makes JS better",
+        actual: `<script>
+            $("#objective").hide(); /* Code */
+            </script>
+            <div id="objective" class="fs-4 fw-bold text-center"> Every Bee is Above the Sea </div>`,
+        editable: `// If intel is $("#objective")...\n//Then what's your JS\n`,
+        build: function(code) {
+            return this.actual.replace("/* Code */", code);
+        }
+    },
+    {
+        id: 6,
         mode: "javascript",
         task: "Find the answer and Fill in the blank!",
         actual: `<script>
@@ -56,7 +88,7 @@ let questions = [
         }
     },
     {
-        id: 5,
+        id: 7,
         mode: "css",
         task: "Coding Club is the place where human intellect grows with coding. One logo will flip on the y-axis when hovered, make the other one too to do so.",
         actual: `<style>
@@ -71,7 +103,8 @@ let questions = [
             code = code.substring(code.lastIndexOf('{') + 1, code.lastIndexOf('}'));
             return this.actual.replace("/* Code */", code);
         }
-    }
+    },
+    
 ]
 
 $(document).ready(() => {
@@ -82,8 +115,10 @@ $(document).ready(() => {
     // Question Rendering
     renderQuestion(questions[next].id, questions[next].task, questions[next].actual);
 
-    let setmode = questions[next].mode == "css";
-    $("#code-mode").text(setmode ? "CSS" : "JS");
+    let setmode = questions[next].mode == "css" ? 1 : questions[next].mode == "xml" ? 2 : 3;
+    $("#code-mode").text(setmode == 1 ? "CSS" : setmode == 2 ? "HTML" : "JS");
+    next == 0 ? $("#back").hide() : $("#back").show();
+    next == 4 ? $("#next").hide() : $("#next").show();    
     let cm = applyCodeMirror(questions[next].editable, questions[next].mode, "dracula");
 
     // Toggle Background
@@ -110,23 +145,25 @@ $(document).ready(() => {
 
     // Move Through Questions
     function moveQuestion() {
+        next == 0 ? $("#back").hide() : $("#back").show();
+        next == 6 ? $("#next").hide() : $("#next").show();
         if(done[next])  $("#q-done").text("DONE");
         cm.setOption("mode", questions[next].mode);
-        let setmode = questions[next].mode == "css";
-        $("#code-mode").text(setmode ? "CSS" : "JS");
+        let setmode = questions[next].mode == "css" ? 1 : questions[next].mode == "javascript" ? 2 : 3;
+        $("#code-mode").text(setmode == 1 ? "CSS" : setmode == 2 ? "JS" : "HTML");
         cm.getDoc().setValue(questions[next].editable);
         renderQuestion(questions[next].id, questions[next].task, questions[next].actual);
     }
 
     // Back Question
     $("#back").on("click", function() {
-        next = (next - 1) % 5;
+        next = (next - 1) % 7;
         moveQuestion();
     })
 
     // Next Question
     $("#next").on("click", function() {
-        next = (next + 1) % 5;
+        next = (next + 1) % 7;
         moveQuestion();
     })
 
@@ -152,5 +189,5 @@ function renderQuestion(id, task, render)   {
     $("#q-id").text("Q" + id + " .");
     $("#q-val").text(task);
     $("#work-space").html(render);
-    $("#q-num").html("&nbsp;&nbsp;&nbsp;Question " + id + " of 5");
+    $("#q-num").html("&nbsp;&nbsp;&nbsp;Question " + id + " of 7");
 }
